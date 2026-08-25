32e4bd17d886f97a889aa1f6eb9789f534e6dca0
William Li <williamli.sci@gmail.com>
2026-08-25 19:35:09 +0700

    Updates

--- assets/kien-nhan-DbhkAIEP.js
+++ assets/kien-nhan-DbhkAIEP.js
@@ -9,6 +9,5 @@ excerpt: >-
   Bài học sâu sắc về sự kiên nhẫn, nhắc nhở ta cần rèn luyện từ những điều nhỏ
   bé và biết hy sinh lợi ích trước mắt vì tương lai.
 ---
-- bơi ở vũng nước trâu nằm chưa thông thạo thì đừng vội bơi ở biển lớn. hãy kiên nhẫn.\r
-- nếu bạn ăn gốc măng thì rất dễ dàng, nhưng bạn sẽ mất đi cây tre trong tương lai. hãy kiên nhẫn.\r
+- nếu bạn ăn gốc măng thì rất dễ dàng, nhưng bạn sẽ mất đi cây tre trong tương lai.
 `;export{e as default};
