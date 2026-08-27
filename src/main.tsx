5fe770a0abaa9095d40821022260d82a95aeae7e
William Li <williamli.sci@gmail.com>
2026-08-28 06:41:36 +0700

    toi uu hieu nang

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root") as HTMLElement;
+
+createRoot(rootElement).render(
 	<StrictMode>
 		<App />
 	</StrictMode>,
