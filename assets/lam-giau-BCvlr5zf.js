4aca712a0ce870dd8334f6efa09bbba98fafd784
William Li <williamli.sci@gmail.com>
2026-08-29 03:19:38 +0700

    Updates

---
+++ assets/lam-giau-BCvlr5zf.js
@@ -0,0 +1,9 @@
+var e=`---
+title: Lam Giau
+date: '2026-08-16'
+---
+- giá trị thặng dư là phần giá trị dôi ra ngoài giá trị sức lao động do người lao động tạo ra nhưng bị chủ tư bản chiếm đoạt.
+- Yếu tố tạo nên sự khác biệt giữa người giàu và người nghèo là tốc độ tích lũy giá trị thặng dư.
+- Người nghèo: dùng thời gian và sức lực để đổi lấy tiền lương. Phần lớn thu nhập kiếm được dùng để chi trả chi phí sinh hoạt hàng ngày (ăn uống, nhà ở, y tế). Gần như không có thặng dư để tái đầu tư, hoặc tốc độ tích lũy thặng dư quá nhỏ so với tốc độ trượt giá và chi phí cuộc sống.
+- Người giàu: Họ dùng vốn để mua máy móc, nguyên liệu và thuê lao động tạo ra sản phẩm. Giá trị thặng dư thu về không dùng hết cho tiêu dùng cá nhân mà được bơm ngược lại vào chu trình sản xuất để mở rộng quy mô. Vòng quay tư bản càng nhanh, lượng giá trị thặng dư sinh ra trong một đơn vị thời gian càng lớn, tạo ra khoảng cách giàu nghèo ngày càng nới rộng.
+`;export{e as default};
