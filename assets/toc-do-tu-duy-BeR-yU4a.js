acff88ec85e6d0e0c64db6c87268aecd999bba82
William Li <williamli.sci@gmail.com>
2026-09-05 04:28:50 +0700

    Updates

--- assets/toc-do-tu-duy-BeR-yU4a.js
+++
@@ -1,6 +0,0 @@
-var e=`---
-title: Toc Do Tu Duy
-date: '2026-08-16'
----
-- muốn tư duy nhanh thì đừng làm não bị quá tải thông tin.\r
-`;export{e as default};
--- assets/van-su-khoi-dau-nan-C_XLXzzl.js
+++
@@ -1,9 +0,0 @@
-var e=`---
-title: Van Su Khoi Dau Nan
-date: '2026-08-25'
----
-- Vạn sự bao giờ cũng khởi đầu nan.\r
-- tránh chủ nghĩa hoàn hảo.\r
-- tránh quá tải thông tin.\r
-- Sự kiên trì không biết mệt mỏi.
-`;export{e as default};
