8d79f86e6ee6729a80e117be057297c01b9ac29f
William Li <williamli.sci@gmail.com>
2026-09-08 06:26:58 +0700

    Updates

---
+++ assets/tu-cai-tien-de-quy-DW31UOnv.js
@@ -0,0 +1,6 @@
+var e=`---
+title: Tu Cai Tien De Quy
+date: '2026-09-06'
+---
+- AI mạnh hơn giúp con người nghiên cứu AI nhanh hơn, thế hệ AI tiếp theo lại tiếp tục nâng cao hiệu suất nghiên cứu, từ đó đẩy nhanh quá trình phát triển các thế hệ sau. khả năng này gọi là “recursive self-improvement”, tức quá trình tự cải tiến mang tính đệ quy.
+`;export{e as default};
