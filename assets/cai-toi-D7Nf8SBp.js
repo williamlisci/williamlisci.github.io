ba0e04372c4d3ce2c4c135fa7bf38f9b5fa1a8e4
William Li <williamli.sci@gmail.com>
2026-09-15 23:34:36 +0700

    Updates

--- assets/cai-toi-D7Nf8SBp.js
+++
@@ -1,8 +0,0 @@
-var e=`---
-title: Cai Toi
-date: '2026-09-15'
----
-- cái tôi (bản ngã, sự ích kỷ, cố chấp) chính là gốc rễ của mọi mâu thuẫn, xung đột và đau khổ trong cuộc sống.\r
-- người nào có thể chiến thắng được cái tôi của chính mình thì đời người không còn kẻ thù nữa. cái tôi là kẻ thù lớn nhất.\r
-- Người biết hạ cái tôi xuống sẽ nhìn nhận sự việc bằng sự bao dung, lắng nghe và thấu hiểu.\r
-`;export{e as default};
---
+++ assets/cai-toi-Y6xpa6oG.js
@@ -0,0 +1,8 @@
+var e=`---
+title: Cai Toi
+date: '2026-09-15'
+---
+- cái tôi (bản ngã, sự ích kỷ, cố chấp) chính là gốc rễ của mọi mâu thuẫn, xung đột và đau khổ trong cuộc sống.
+- Kiềm chế cái tôi là cảnh giới cao nhất của sự trưởng thành, khi ấy thế giới xung quanh không còn ai là kẻ thù của bạn nữa.
+- Người biết hạ cái tôi xuống sẽ nhìn nhận sự việc bằng sự bao dung, lắng nghe và thấu hiểu.
+`;export{e as default};
