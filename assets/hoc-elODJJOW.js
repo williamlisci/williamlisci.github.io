57c438f53f15de29024daca167b19d3f4f63654d
William Li <williamli.sci@gmail.com>
2026-09-15 22:16:38 +0700

    Updates

---
+++ assets/hoc-elODJJOW.js
@@ -0,0 +1,7 @@
+var e=`---
+title: Hoc
+date: '2026-08-16'
+---
+- không ai sinh ra đã biết tất cả mọi thứ; con người cần rất nhiều thời gian để trưởng thành và học hỏi. 段永平.
+- học để nhớ và vận dụng khi cần. muốn nhớ thì phải hiểu. muốn hiểu thì phải hỏi.
+`;export{e as default};
