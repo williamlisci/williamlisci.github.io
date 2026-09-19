86bd932f66d3f8632e989eb00ae7ead05396bbc8
William Li <williamli.sci@gmail.com>
2026-09-20 03:30:53 +0700

    Updates

--- assets/don-bay-tai-chinh-CCurAjwu.js
+++ assets/don-bay-tai-chinh-CCurAjwu.js
@@ -1,8 +1,9 @@
 var e=`---
-title: Rui Ro
+title: Don Bay Tai Chinh
 date: '2026-08-16'
 ---
-- Nếu biết trước nơi mình sẽ chết, đừng đến đó.
+- Đối với các công ty tốt, khủng hoảng thực chất là một cơ hội. Chúng tôi không vay vốn và có dòng tiền dồi dào , vì vậy mọi cuộc khủng hoảng đều là cơ hội đối với chúng tôi. 段永平.
+- Ưu điểm của vay nợ là nó cho phép phát triển nhanh hơn. Ưu điểm của việc không mắc nợ là bạn có thể sống lâu hơn. 段永平.
+- Dùng đòn bẩy tài chính (vay nợ) có thể khuếch đại lợi nhuận. Nhưng trong khủng hoảng, nợ chính là ác mộng. Những công ty vay nợ nhiều sẽ đối mặt với áp lực trả gốc và lãi, dễ dẫn đến phá sản do đứt gãy dòng tiền.
 - Chúng tôi không có các khoản vay có lãi suất . vì vậy chúng tôi sẽ không bao giờ sụp đổ do dòng tiền bị gián đoạn. hầu hết các doanh nghiệp thất bại vì họ vay quá nhiều tiền . Về cơ bản, công ty chúng tôi không có khoản vay nào, mặc dù các ngân hàng đã cấp cho chúng tôi một hạn mức tín dụng đáng kể từ lâu. Tốt nhất là không nên vay tiền nếu bạn không chắc mình có thể trả nợ. Một số cơ hội chắc chắn sẽ bị bỏ lỡ, nhưng miễn là bạn nắm bắt được những cơ hội đúng đắn, thế là đủ. Có lẽ đó là lý do tại sao hầu hết các đối thủ cạnh tranh trước đây của chúng ta đã biến mất trong khi chúng ta vẫn còn tồn tại. 段永平.
-- Rủi ro ≈ Xác suất xảy ra×Mức độ hậu quả
 `;export{e as default};
---
+++ assets/du-phong-rui-ro-BWqcpPTD.js
@@ -0,0 +1,11 @@
+var e=`---
+title: du phong Rui Ro
+date: '2026-08-16'
+---
+- Nếu biết trước nơi mình sẽ chết, đừng đến đó.
+- Dự phòng rủi ro vì cuộc sống là sự vô thường. 
+- trong Phật giáo, anicca (vô thường) là một đặc tính căn bản của các pháp hữu vi. Vấn đề không chỉ nằm ở việc thế giới thay đổi, mà còn ở chỗ con người thường muốn những thứ vốn biến đổi phải trở nên bất biến. Khoảng cách giữa thực tại và kỳ vọng đó tạo ra đau khổ.
+- Dự phòng không tạo ra một cuộc đời không có sóng gió. Nó tạo ra khả năng chống chịu trước sóng gió.
+- giá trị lớn nhất của dự phòng không phải là ngăn biến cố xảy ra, mà là tạo cho con người thời gian và không gian để phản ứng một cách tỉnh táo.
+- đừng cố tìm kiếm sự chắc chắn, hãy xây dựng năng lực thích nghi.
+`;export{e as default};
