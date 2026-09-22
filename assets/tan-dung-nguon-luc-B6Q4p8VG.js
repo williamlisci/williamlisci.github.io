6149e75322266127eb66a47acb2967f0249d9afe
William Li <williamli.sci@gmail.com>
2026-09-23 02:01:20 +0700

    Updates

---
+++ assets/tan-dung-nguon-luc-B6Q4p8VG.js
@@ -0,0 +1,10 @@
+var e=`---
+title: Tan Dung Nguon Luc
+date: '2026-09-22'
+---
+- “Với những gì đang có, tôi có thể tiến được bao xa?”\r
+- Một trong những trở ngại lớn nhất của con người là tâm lý trì hoãn hành động cho đến khi điều kiện đủ tốt. Vấn đề là “hoàn cảnh hoàn hảo” gần như không tồn tại. Hãy hành động với nguồn lực hiện có, sau đó dùng kết quả của hành động để tạo thêm nguồn lực.\r
+- Không có nguồn lực nào hoàn toàn vô giá trị trong mọi hoàn cảnh. Ví dụ, một kỹ năng tưởng như không liên quan trong lĩnh vực này có thể trở thành lợi thế trong lĩnh vực khác.\r
+- Khi nguồn lực hạn chế, mỗi quyết định đều có chi phí cơ hội rõ ràng. Người có ít nguồn lực thường đặc biệt cần tối ưu hiệu suất trên mỗi đơn vị nguồn lực.\r
+- năng lực không chỉ là khả năng đạt mục tiêu trong điều kiện thuận lợi. Nó còn là khả năng tạo ra tiến bộ trong điều kiện không hoàn hảo. Ví dụ trong 1 ván bài, Con người không được lựa chọn toàn bộ quân bài mình nhận được. Nhưng một phần rất quan trọng của năng lực nằm ở cách mình sắp xếp, kết hợp và sử dụng những quân bài đó.\r
+`;export{e as default};
