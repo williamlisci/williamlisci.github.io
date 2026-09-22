4f93b56b3853e853cd72439ee088daf119899ffe
William Li <williamli.sci@gmail.com>
2026-09-23 03:13:51 +0700

    Updates

---
+++ assets/tu-duy-he-thong-CgxhKPq7.js
@@ -0,0 +1,6 @@
+var e=`---
+title: Tu Duy He Thong
+date: '2026-09-22'
+---
+- Khả năng nhìn ra các điểm chung và quy luật ẩn giấu đằng sau những dữ liệu rời rạc chính là bản chất của tư duy hệ thống.\r
+`;export{e as default};
