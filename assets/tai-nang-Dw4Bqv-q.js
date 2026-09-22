549e1c44ba5eeec186f5c3f84225a474b2612fa9
William Li <williamli.sci@gmail.com>
2026-09-23 03:31:28 +0700

    Updates

---
+++ assets/tai-nang-Dw4Bqv-q.js
@@ -0,0 +1,9 @@
+var e=`---
+title: Tai Nang
+date: '2026-09-22'
+---
+- Tài năng là tiềm năng được bồi dưỡng.\r
+- không có một tài năng nào có thể tỏa sáng nếu thiếu đi quá trình rèn luyện lâu dài.\r
+- mỗi người đều mang trong mình những hạt giống tiềm ẩn chờ được đánh thức.\r
+- Tiềm năng muốn biến thành tài năng thực sự (thành tựu, kỹ năng xuất chúng) bắt buộc phải trải qua quá trình bồi dưỡng nghiêm túc. không nên tự giới hạn bản thân bằng suy nghĩ "mình không có năng khiếu". Sự nỗ lực và phương pháp đúng có thể bù đắp và phát triển năng lực của một người.\r
+`;export{e as default};
